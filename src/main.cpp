0169c119e3d9983cadb3e3c77a720f0f05dffe68
Marcin Praski <marcin.praski@live.com>
2018-12-02 23:30:12 +0000

    annotated main

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@
 #include "example/onemax_fitness_evaluation.cpp"
 
 int main() {
+  /*
+   * Core framework configuration is represented by
+   * system_properties struct. See definition in core.hpp
+   */
   system_properties system_props;
   system_props.population_size = 100;
   system_props.individual_size = 10;
@@ -34,12 +38,36 @@ int main() {
   system_props.is_individual_reporter_active = true;
   system_props.individual_reporter_log = "individual_reporter.csv";
 
+  /*
+   * All other values defined by the user and somehow needed,
+   * for instance in custom genetic operators (see operators/sequence_individual_initialization.hpp)
+   * can be put in the user_properties map. It's a std::unordered_map<std::string, std::any>, so
+   * any value can be stored (has to be cast to appropriate type using std::any_cast)
+   */
   user_properties user_props;
   user_props[constants::POSSIBLE_VALUES_KEY] =
       std::vector<bool> { true, false };
   user_props[constants::STABLE_REQUIRED_KEY] = std::size_t { 10 };
   user_props[constants::MINIMUM_AVERAGE_KEY] = 8;
 
+  /*
+   * The global_model_driver class encapsulates the global model genetic algorithm.
+   * When constructing a used must supply a list of types, which are as follow:
+   * 1. individual type
+   * 2. fitness value type
+   * 3. fitness evaluation operator type
+   * 4. initialization operator type
+   * 5. crossover operator type
+   * 6. mutatation operator type
+   * 7. parent selection operator type
+   * 8. survival selection operator type
+   * 9. elitist operator type
+   * 10. termination check operator type
+   *
+   * All of the 'operator' types must have a constructor
+   * accepting a shared_config (shared pointer to the configuration object),
+   * which allows to pass necessary data to the genetic operator functors
+   */
   global_model_driver<sequence<bool>, int, onemax_fitness_evaluation,
       sequence_individual_initialization<bool, int>,
       sequence_individual_crossover<bool, int>, bitstring_mutation,
